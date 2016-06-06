1f7829dbf424b300199478d7397e3daf05226bcc
AhSaN <AhSaN@Ahsan>
2016-06-06 17:16:30 +0500

    Change-Id: I02c6be235a621f7383729ef24bd78955d1fc4925 Signed-off-by: AhSaN <AhSaN@Ahsan>

---
+++ src/MAIN.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MAIN {
+	
+	public static void main(String args[]){
+	  int x, y, z;
+      System.out.println("Enter two integers to calculate their sum ");
+      Scanner in = new Scanner(System.in);
+      x = in.nextInt();
+      y = in.nextInt();
+      z = x + y;
+      System.out.println("add of entered integers = "+z);
+  
+	
+}
+}
